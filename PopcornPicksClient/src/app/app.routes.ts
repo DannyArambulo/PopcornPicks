b774860300c9f62e5c45bc5378baca22dd501c03
nathan kook <112519113+nathankook@users.noreply.github.com>
2024-10-12 16:13:12 -0700

    started register page

--- PopcornPicksClient/src/app/app.routes.ts
+++ PopcornPicksClient/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {path: 'logincomponent', component: LoginComponent},
   {path: 'welcomecomponent', component: WelcomeComponent},
+  {path: 'registercomponent', component: RegisterComponent},
   {path: '', redirectTo: 'welcomecomponent', pathMatch: 'full'}
 ];
 
