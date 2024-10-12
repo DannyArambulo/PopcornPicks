9dc9b295d26a6959b83580fd3328c95e4b9afb58
Waleed <wanade01@gmail.com>
2024-10-12 09:07:25 -0700

    Completed Login Page

--- PopcornPicksClient/src/app/login/login.component.ts
+++ PopcornPicksClient/src/app/login/login.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormField} from '@angular/material/form-field';
+import {MatLabel} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {merge} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import { MatCardActions } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormField, MatLabel, MatInputModule, FormsModule, ReactiveFormsModule, MatCardActions, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
 
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 }
