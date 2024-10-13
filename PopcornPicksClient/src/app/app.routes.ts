608bb4d5d69c39d7adf79d83f042019b83955e5a
nathan kook <112519113+nathankook@users.noreply.github.com>
2024-10-13 16:29:25 -0700

    Added routes to home, favorites, history

--- PopcornPicksClient/src/app/app.routes.ts
+++ PopcornPicksClient/src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+
 
 export const routes: Routes = [
   {path: 'logincomponent', component: LoginComponent},
   {path: 'welcomecomponent', component: WelcomeComponent},
   {path: 'registercomponent', component: RegisterComponent},
+  {path: 'homecomponent', component: HomeComponent },
+  {path: 'favoritescomponent', component: FavoritesComponent }, 
+  {path: 'historycomponent', component: HistoryComponent }, 
   {path: '', redirectTo: 'welcomecomponent', pathMatch: 'full'}
 ];
 
