ccf8c78834b03036c2141bc4dabfca239f96cc6e
Waleed <wanade01@gmail.com>
2024-10-16 15:38:56 -0700

    set up footer component

--- PopcornPicksClient/src/app/app.component.ts
+++ PopcornPicksClient/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule, MatToolbarModule, MatIconModule, MatButtonModule, NavBarComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule, MatToolbarModule, MatIconModule, MatButtonModule, NavBarComponent, FooterComponent, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
