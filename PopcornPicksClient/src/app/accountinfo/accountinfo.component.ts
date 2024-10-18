6f85baa1edf83fb6a29640e0511e4b990758a724
Waleed <wanade01@gmail.com>
2024-10-17 23:11:57 -0700

    partially completed account info component, was able to hide search bar.

---
+++ PopcornPicksClient/src/app/accountinfo/accountinfo.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormField} from '@angular/material/form-field';
+import {MatLabel} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {merge} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import { MatCardActions } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-accountinfo',
+  standalone: true,
+  imports: [MatCardModule, MatFormField,MatLabel,MatInputModule,MatCardActions,MatButtonModule],
+  templateUrl: './accountinfo.component.html',
+  styleUrl: './accountinfo.component.css'
+})
+export class AccountinfoComponent {
+
+}
