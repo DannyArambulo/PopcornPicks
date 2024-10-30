dfdc828a273609b747519aa045868ea0cedac1fa
nathan kook <112519113+nathankook@users.noreply.github.com>
2024-10-29 18:55:33 -0700

    Started working on auth0 and authbuttoncomponent

--- PopcornPicksClient/src/main.ts
+++ PopcornPicksClient/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [appConfig.providers,
+    provideAuth0({
+      domain: 'dev-qk32cwgnciwik2to.us.auth0.com',
+      clientId: 'N7Eazf21KpCFCM0y4Cuk8xvLotiqEf7P',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ]
+});
