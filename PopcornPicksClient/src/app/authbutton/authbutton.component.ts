3ad36ce7b1091f5a418355b88a7d29cc4f75c992
Waleed <wanade01@gmail.com>
2024-11-03 15:18:40 -0800

    Navbar now changes state depending on if user is logged in or not.

--- PopcornPicksClient/src/app/authbutton/authbutton.component.ts
+++ PopcornPicksClient/src/app/authbutton/authbutton.component.ts
@@ -3,13 +3,15 @@ import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button'
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-auth-button',
-  imports: [CommonModule, MatButtonModule ],
+  imports: [CommonModule, MatButtonModule, NavBarComponent, MatMenuModule],
   template: `
     <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
-      <button mat-raised-button color="warn" (click)="auth.logout({ logoutParams: { returnTo: document.location.origin } })">
+      <button mat-menu-item color="warn" (click)="auth.logout({ logoutParams: { returnTo: document.location.origin } })">
         Log out
       </button>
     </ng-container>
