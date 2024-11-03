0983382748c7d4c8936c3aa1cf16759036c4c59b
Waleed <wanade01@gmail.com>
2024-11-03 15:33:53 -0800

    Fixed logo going to homecomponent after login.

--- PopcornPicksClient/src/app/navmenu/navmenu.component.ts
+++ PopcornPicksClient/src/app/navmenu/navmenu.component.ts
@@ -4,11 +4,12 @@ import {MatButtonModule} from '@angular/material/button';
 import { AuthService } from '@auth0/auth0-angular';
 import { MatIcon } from '@angular/material/icon';
 import { AuthbuttonComponent } from '../authbutton/authbutton.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navmenu',
   standalone: true,
-  imports: [MatMenuModule, MatButtonModule, MatIcon, AuthbuttonComponent],
+  imports: [MatMenuModule, MatButtonModule, MatIcon, AuthbuttonComponent, RouterModule, RouterOutlet],
   templateUrl: './navmenu.component.html',
   styleUrl: './navmenu.component.css'
 })
