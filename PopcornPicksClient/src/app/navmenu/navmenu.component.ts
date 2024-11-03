6568b130a8d1066cd7c7fa21ea8b3b06cfcf4300
Waleed <wanade01@gmail.com>
2024-11-03 15:18:40 -0800

    Navbar now changes state depending on if user is logged in or not.

---
+++ PopcornPicksClient/src/app/navmenu/navmenu.component.ts
@@ -0,0 +1,18 @@
+import {Component, inject} from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { AuthService } from '@auth0/auth0-angular';
+import { MatIcon } from '@angular/material/icon';
+import { AuthbuttonComponent } from '../authbutton/authbutton.component';
+
+@Component({
+  selector: 'app-navmenu',
+  standalone: true,
+  imports: [MatMenuModule, MatButtonModule, MatIcon, AuthbuttonComponent],
+  templateUrl: './navmenu.component.html',
+  styleUrl: './navmenu.component.css'
+})
+export class NavmenuComponent {
+  private auth = inject(AuthService);
+  user$ = this.auth.user$;
+}
