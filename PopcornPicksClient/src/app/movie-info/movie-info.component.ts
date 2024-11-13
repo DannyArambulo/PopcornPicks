5ac8c0b6cff3becdc399ba5093db7ac6eba27848
Waleed <wanade01@gmail.com>
2024-11-12 16:14:50 -0800

    Added more elements to movieinfocomponent, replaced placeholder logo with new logo.

--- PopcornPicksClient/src/app/movie-info/movie-info.component.ts
+++ PopcornPicksClient/src/app/movie-info/movie-info.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-movie-info',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatFormField, MatButtonModule],
   templateUrl: './movie-info.component.html',
   styleUrl: './movie-info.component.css'
 })
