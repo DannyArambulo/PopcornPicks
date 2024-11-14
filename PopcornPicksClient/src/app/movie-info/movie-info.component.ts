00d3e699f78064d5d2c750aa89f4c2ef8cfbfe84
Waleed <wanade01@gmail.com>
2024-11-13 19:06:35 -0800

    Added rating to movie-info

--- PopcornPicksClient/src/app/movie-info/movie-info.component.ts
+++ PopcornPicksClient/src/app/movie-info/movie-info.component.ts
@@ -1,15 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { MatFormField } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-info',
   standalone: true,
-  imports: [MatCardModule, MatFormField, MatButtonModule],
+  imports: [MatCardModule, MatFormField, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './movie-info.component.html',
   styleUrl: './movie-info.component.css'
 })
 export class MovieInfoComponent {
 
+  numStars: number[] = [1,2,3,4,5,6,7,8,9,10];
+  rating: number = 0;
+
+
+  starRating(score: number) {
+    this.rating = score;
+    console.log("Rating: ", this.rating)
+  }
+
+  showStar(id:number){
+    if (this.rating >= id + 1) {
+      return 'star';
+    }
+
+    else {
+      return 'star_border';
+    }
+  }
 }
