3a2dfd147e5cd7ab5606ffe20d7e0dfdb1d01788
Waleed <wanade01@gmail.com>
2024-11-16 15:22:12 -0800

    Got each search result to have a link that leads to the movie page, still trying to figure out how to get movie info.

--- PopcornPicksServer/search.py
+++ PopcornPicksServer/search.py
@@ -9,11 +9,18 @@ TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 
 search = Flask(__name__)
 CORS(search, resources={r"/search/*": {"origins": "http://localhost:4200"}}) 
+CORS(search, resources={r"/movie/*": {"origins": "http://localhost:4200"}}) 
 
 @search.route('/search', methods=['GET'])
 def search_movies():
     query = request.args.get('query')
     response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={query}')
+    return jsonify(response.json())
+
+@search.route('/movie', methods=['GET'])
+def getMovie():
+    movieId = request.args.get('id')
+    response = requests.get(f'https://api.themoviedb.org/3/movie/?api_key={TMDB_API_KEY}&movie_id={movieId}')
     return jsonify(response.json())
 
 if __name__ == '__main__':
