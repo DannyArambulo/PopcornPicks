e46ce99f2cd69d627312c918c98764b552e3868c
Waleed <wanade01@gmail.com>
2024-11-20 12:42:32 -0800

    Added some functions to buttons in MovieInfo, save button function will be added later.

--- PopcornPicksServer/search.py
+++ PopcornPicksServer/search.py
@@ -4,13 +4,19 @@ import os
 import requests
 import sys
 from dotenv import load_dotenv
+from flaskext.mysql import MySQL
 
 load_dotenv()
 TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 
 search = Flask(__name__)
 CORS(search, resources={r"/search/*": {"origins": "http://localhost:4200"}}) 
-CORS(search, resources={r"/movie/*": {"origins": "http://localhost:4200"}}) 
+CORS(search, resources={r"/movie/*": {"origins": "http://localhost:4200"}})
+
+search.config['MYSQL_HOST'] = 'localhost'
+search.config['MYSQL_USER'] = 'root'
+search.config['MYSQL_PASSWORD'] = '1234'
+search.config['MYSQL_DB'] = 'popcornpicks'   
 
 @search.route('/search', methods=['GET'])
 def search_movies():
@@ -24,6 +30,10 @@ def getMovie():
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}')
     print(response.text)
     return jsonify(response.json())
+
+def test_sql():
+    msg = ""
+    cursor = MySQL.con
 
 if __name__ == '__main__':
     search.run(debug=True)
