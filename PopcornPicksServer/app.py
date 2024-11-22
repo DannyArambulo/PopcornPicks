60b8db3384a7542484bc6b49a258c3a7a923b224
Waleed <wanade01@gmail.com>
2024-11-22 15:07:49 -0800

    Created app.py that contains code to create tables for the database. Also added a copy of the database for reference. Also contains base code to for submit button in Movie-Info component.

---
+++ PopcornPicksServer/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask.templating import render_template
+from sqlalchemy import ForeignKey, create_engine
+from dataclasses import dataclass
+
+app = Flask(__name__)
+app.debug = True
+
+#adds config for using a MySQL DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/popcornpicksdb'
+
+#Creating an instance of SQLAlchemy
+db = SQLAlchemy(app)
+
+#Models
+@dataclass
+class Users(db.Model):
+    user_id = db.Column(db.String(45), primary_key=True, unique=True, autoincrement=False)
+    
+@dataclass
+class User_Info(db.Model):
+    user_id = db.Column(db.String(45), ForeignKey(Users.user_id), primary_key=True, nullable=False)
+    genre = db.Column(db.String(20), primary_key=True)
+    
+@dataclass
+class User_Reviews(db.Model):
+    user_id = db.Column(db.String(45), ForeignKey(Users.user_id), primary_key=True, nullable=False)
+    movie_id = db.Column(db.Integer, primary_key=True, unique=True)
+    movie_rating = db.Column(db.Integer)
+    movie_review = db.Column(db.String(10000))
+    
+if __name__ == '__main__':
+    app.run()
