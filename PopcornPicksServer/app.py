f791e5df1f61e1f105bb3856cd28296bc5888185
nathan kook <112519113+nathankook@users.noreply.github.com>
2024-11-23 21:26:48 -0800

    Update app.py
    Added User_Watch_History table and updated movie_id to remove unique=True; working on adding flask-migrate

--- PopcornPicksServer/app.py
+++ PopcornPicksServer/app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template
 from sqlalchemy import ForeignKey, create_engine
 from dataclasses import dataclass
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.debug = True
@@ -12,6 +14,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/popcornpic
 
 #Creating an instance of SQLAlchemy
 db = SQLAlchemy(app)
+
+#Initializes flask-migrate
+migrate = Migrate(app,db)
 
 #Models
 @dataclass
@@ -26,9 +31,16 @@ class User_Info(db.Model):
 @dataclass
 class User_Reviews(db.Model):
     user_id = db.Column(db.String(45), ForeignKey(Users.user_id), primary_key=True, nullable=False)
-    movie_id = db.Column(db.Integer, primary_key=True, unique=True)
+    movie_id = db.Column(db.Integer, primary_key=True)
     movie_rating = db.Column(db.Integer)
     movie_review = db.Column(db.String(10000))
+
+@dataclass
+class User_Watch_History(db.model):
+    user_id = db.Column(db.String(45), ForeignKey(Users.user_id), primary_key=True, nullable=False)
+    movie_id = db.Column(db.Integer, primary_key=True)
+    watch_date = db.Column(db.Date)
+    favorite = db.Column(db.Boolean)
     
 if __name__ == '__main__':
     app.run()
