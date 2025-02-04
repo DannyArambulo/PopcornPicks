837b7ed257990e0767ba513220c080746453f3ef
Waleed <wanade01@gmail.com>
2025-02-03 19:29:29 -0800

    removed forgotpassword and login routes from app.route.ts

--- PopcornPicksClient/src/app/app.routes.ts
+++ PopcornPicksClient/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { HistoryComponent } from './history/history.component';
 import { FirstTimeSetupComponent } from './first-time-setup/first-time-setup.component';
-import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 import { SearchComponent } from './search/search.component';
 import { AccountinfoComponent } from './accountinfo/accountinfo.component';
 import { SearchResultsComponent } from './searchresults/searchresults.component';
@@ -15,13 +13,11 @@ import { RecommendComponent } from './recommend/recommend.component';
 
 
 export const routes: Routes = [
-  {path: 'logincomponent', component: LoginComponent},
   {path: 'welcomecomponent', component: WelcomeComponent},
   {path: 'registercomponent', component: RegisterComponent},
   {path: 'homecomponent', component: HomeComponent }, 
   {path: 'historycomponent', component: HistoryComponent }, 
   {path: 'firsttimesetupcomponent', component: FirstTimeSetupComponent }, 
-  {path: 'forgotpasswordcomponent', component: ForgotpasswordComponent },
   {path: 'searchcomponent', component: SearchComponent }, 
   {path: 'accountinfocomponent', component: AccountinfoComponent }, 
   {path: 'searchresults', component: SearchResultsComponent },
@@ -31,7 +27,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), LoginComponent, WelcomeComponent],
+  imports: [RouterModule.forRoot(routes), WelcomeComponent],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
