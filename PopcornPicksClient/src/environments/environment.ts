0b8ab9932a4e0ec0ceccc71840b60d049056fc79
Waleed <wanade01@gmail.com>
2025-02-26 22:14:14 -0800

    Removing environment files from repository

--- PopcornPicksClient/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: "https://api.popcornpicks.space/",
-    DOMAIN: 'dev-qk32cwgnciwik2to.us.auth0.com', 
-    CLIENT_ID: 'N7Eazf21KpCFCM0y4Cuk8xvLotiqEf7P' 
-};
